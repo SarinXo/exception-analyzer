779565b4ae8f6f49317719b031b2d99ed2a0c487
Ilya <109240426+SarinXo@users.noreply.github.com>
2025-05-01 17:12:02 +0300

    init

---
+++ src/main/java/sarinxo/service/exceptionanalyzer/ExceptionAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package sarinxo.service.exceptionanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionAnalyzerApplication.class, args);
+    }
+
+}
