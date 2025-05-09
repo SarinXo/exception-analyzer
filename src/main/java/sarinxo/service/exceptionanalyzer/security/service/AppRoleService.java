d5849b3e26194aa2656a26b9e73d3df382b0e6a1
Ilya <109240426+SarinXo@users.noreply.github.com>
2025-05-09 13:59:06 +0300

    feat: Api для создания ролей

--- src/main/java/sarinxo/service/exceptionanalyzer/security/service/AppRoleService.java
+++ src/main/java/sarinxo/service/exceptionanalyzer/security/service/AppRoleService.java
@@ -1,12 +1,13 @@
 package sarinxo.service.exceptionanalyzer.security.service;
 
+import sarinxo.service.exceptionanalyzer.security.dto.CreateRoleRequest;
 import sarinxo.service.exceptionanalyzer.security.dto.CreateRoleResponse;
 import sarinxo.service.exceptionanalyzer.security.entity.AppRole;
 
 import java.util.List;
 
 public interface AppRoleService {
-    CreateRoleResponse create(String roleName);
+    CreateRoleResponse create(CreateRoleRequest request);
 
     List<AppRole> findByIds(List<Long> ids);
 
--- src/main/java/sarinxo/service/exceptionanalyzer/security/service/impl/AppRoleServiceImpl.java
+++ src/main/java/sarinxo/service/exceptionanalyzer/security/service/impl/AppRoleServiceImpl.java
@@ -2,6 +2,7 @@ package sarinxo.service.exceptionanalyzer.security.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sarinxo.service.exceptionanalyzer.security.dto.CreateRoleRequest;
 import sarinxo.service.exceptionanalyzer.security.dto.CreateRoleResponse;
 import sarinxo.service.exceptionanalyzer.security.entity.AppRole;
 import sarinxo.service.exceptionanalyzer.security.mapper.AppRoleMapper;
@@ -18,8 +19,8 @@ public class AppRoleServiceImpl implements AppRoleService {
     private final AppRoleMapper mapper;
 
     @Override
-    public CreateRoleResponse create(String roleName) {
-        AppRole role = AppRole.builder().authority(roleName).build();
+    public CreateRoleResponse create(CreateRoleRequest request) {
+        AppRole role = AppRole.builder().authority(request.getRole()).build();
 
         AppRole savedRole = repository.save(role);
 
