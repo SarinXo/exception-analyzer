01e69757b22970ea6b53b54549c1b36ebe69bc5c
Ilya <109240426+SarinXo@users.noreply.github.com>
2025-05-11 21:49:39 +0300

    feat: добавил kafka consumer в проект

---
+++ src/main/java/sarinxo/service/exceptionanalyzer/consumer/KafkaConfig.java
@@ -0,0 +1,50 @@
+package sarinxo.service.exceptionanalyzer.consumer;
+
+import com.sun.jdi.event.ExceptionEvent;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.UUIDDeserializer;
+import org.apache.kafka.common.serialization.UUIDSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ConsumerFactory<UUID, ExceptionDto> consumerFactory(KafkaProperties properties) {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "exception-events");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, UUIDDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+
+        return new DefaultKafkaConsumerFactory<UUID, ExceptionDto>(
+                config,
+                new UUIDDeserializer(),
+                new JsonDeserializer<>(ExceptionDto.class)
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<UUID, ExceptionDto> kafkaListenerContainerFactory(
+            ConsumerFactory<UUID, ExceptionDto> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<UUID, ExceptionDto> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
+
+}
